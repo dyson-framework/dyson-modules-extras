5cad9b9fcd2d3c4989a2a59d666a9fe2ac500457
Daniel Davison <daniel.jj.davison@gmail.com>
2016-11-08 15:38:12 -0500

    add debug extras

---
+++ debug/debug.py
@@ -0,0 +1,7 @@
+from dyson.utils.module import DysonModule
+
+
+class DebugModule(DysonModule):
+    def run(self, webdriver, params):
+        if 'var' in params:
+            print("DEBUG: %s" % params['var'])
