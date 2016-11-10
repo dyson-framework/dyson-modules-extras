dfaf7cb6ed0468b40a9a66d6c5dc77fb11b4b04e
Daniel Davison <daniel.jj.davison@gmail.com>
2016-11-09 23:51:31 -0500

    Return from debug

--- debug/debug.py
+++ debug/debug.py
@@ -5,5 +5,7 @@ class DebugModule(DysonModule):
     def run(self, webdriver, params):
         if 'var' in params:
             print("DEBUG: %s" % params['var'])
+            return params['var']
         if 'msg' in params:
             print("DEBUG: %s" % params['msg'])
+            return params['var']
