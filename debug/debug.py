184a53b7557f454bea8ddcd64cdcb57b497121b1
Daniel Davison <daniel.jj.davison@gmail.com>
2016-11-12 17:10:39 -0500

    debug anyway if it can't find anything

--- debug/debug.py
+++ debug/debug.py
@@ -3,9 +3,12 @@ from dyson.utils.module import DysonModule
 
 class DebugModule(DysonModule):
     def run(self, webdriver, params):
-        if 'var' in params:
-            print("DEBUG: %s" % params['var'])
-            return params['var']
-        if 'msg' in params:
-            print("DEBUG: %s" % params['msg'])
-            return params['var']
+        try:
+            if 'var' in params:
+                print("DEBUG: %s" % params['var'])
+                return params['var']
+            if 'msg' in params:
+                print("DEBUG: %s" % params['msg'])
+                return params['var']
+        except:
+            print(params)
